af573a8ecb115e419e5c200a8ecf475ae45c89e5
haha44444 <jk077088@outlook.com>
2023-08-18 18:18:59 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import App from './App.vue'
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 Vue.use(VueMeta, {
   refreshOnceOnNavigation: true
 })
